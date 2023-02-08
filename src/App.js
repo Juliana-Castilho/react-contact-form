32db80ecd1415afdada518cf9fff40355c6fda0e
Juliana-Castilho <julie.castilho@gmail.com>
2023-02-07 22:44:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ContactForm from './components/ContactForm/ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <ContactForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
+import { init } from 'emailjs-com';
+import './ContactForm.styles.css';
+
+const ContactForm = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+    const [emailSent, setEmailSent] = useState(false);
+
+    const isValidEmail = email => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return regex.test(String(email).toLowerCase());
+    }
+
+    init('user_id');
+
+    const submit = () => {
+
+        if (name && email && phone && message && isValidEmail === true) {
+
+            const serviceId = 'service_n5w37sf';
+            const templateId = 'template_53rk0qg';
+            // const userId = 'user_id';
+            const templateParams = {
+                name,
+                email,
+                phone,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams)
+                .then(response => console.log(response))
+                .then(error => console.log(error))
+
+            setName('');
+            setEmail('');
+            setPhone('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert("Por favor, preencha todos os campos corretamente.")
+        }
+    }
+
+
+    return (
+        <div className="form">
+            <div className="contact-form" >
+                <div className="input-info">
+                    <label htmlFor="name">Seu nome</label>
+                    <input type="text" placeholder="Nome completo" name='name' value={name} onChange={e => setName(e.target.value)} />
+
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" placeholder="nome@email.com.br" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <label htmlFor="phone">Telefone</label>
+                    <input type="text" placeholder="(11) 99999-9999" name="phone" value={phone} onChange={e => setPhone(e.target.value)} />
+                </div>
+                <div className="input-message">
+                    <label htmlFor="message">Mensagem</label>
+                    <textarea placeholder="Deixe sua mensagem" name="message" value={message} onChange={e => setMessage(e.target.value)} ></textarea>
+                </div>
+            </div>
+            <div className="input-sent">
+                <button onClick={submit}>Enviar mensagem</button>
+
+                <span className={emailSent ? 'visible' : null}>Obrigado por sua mensagem. Entraremos em contato em breve.</span>
+            </div>
+        </div>
+    );
+};
+
+export default ContactForm;
